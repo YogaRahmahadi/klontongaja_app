c0d32a9e5a4d8eedca863f920dab01cc52f38d87
YogaRahmahadi <yogarahmahadi1234@gmail.com>
2022-12-01 10:34:38 +0700

    Update Staff Page

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
         <ul class="sidebar-menu" data-widget="tree">
             <li>
                 <a href="{{ route('dashboard') }}">
-                    <i class="fa fa-home"></i> <span>Dashboard</span>   
+                    <i class="fa fa-home"></i> <span>Dashboard</span>
                 </a>
             </li>
 
@@ -34,7 +34,7 @@
                     <a href="{{ route('supplier.index') }}">
                         <i class="fa fa-industry"></i> <span>Supplier</span>
                     </a>
-                </li>      
+                </li>
                 <li>
                     <a href="{{ route('pembelian.index') }}">
                         <i class="fa fa-cube"></i> <span>Barang Masuk</span>
@@ -45,31 +45,53 @@
                         <i class="fa fa-cart-arrow-down"></i> <span>Transaksi</span>
                     </a>
                 </li>
-                {{-- <li>
-                    <a href="{{ route('laporan.index') }}">
-                        <i class="fa fa-file-pdf-o"></i> <span>Laporan</span>
-                    </a>
-                </li> --}}
                 <li>
                     <a href="{{ route('penjualan.index') }}">
-                        <i class="fa fa-file-pdf-o"></i> <span>Laporan</span>
+                        <i class="fa fa-file-pdf-o"></i> <span>Laporan Penjualan</span>
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('penjualan.index') }}" class="fa fa-arrow-left">
-                        <i onclick="$('#logout-form').submit()"></i><span>Logout</span>
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        <i class="fa fa-minus" aria-hidden="true"> </i><span>Logout</span>
                     </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </li>
             @else
                 <li>
-                    <a href="{{ route('transaksi.index') }}">
-                        <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Aktif</span>
+                    <a href="{{ route("user.profil") }}">
+                        <i class="fa fa-user"></i> <span>Profil</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('produk.index') }}">
+                        <i class="fa fa-cube"></i> <span>Produk</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('pembelian.index') }}">
+                        <i class="fa fa-cube"></i> <span>Barang Masuk</span>
                     </a>
                 </li>
                 <li>
                     <a href="{{ route('transaksi.baru') }}">
                         <i class="fa fa-cart-arrow-down"></i> <span>Transaksi Baru</span>
                     </a>
+                </li>
+                <li>
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        <i class="fa fa-minus" aria-hidden="true"> </i><span>Logout</span>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </li>
             @endif
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\{
     UserController,
 };
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,25 +40,11 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/kategori/data', [KategoriController::class, 'data'])->name('kategori.data');
         Route::resource('/kategori', KategoriController::class);
 
-        Route::get('/produk/data', [ProdukController::class, 'data'])->name('produk.data');
-        Route::post('/produk/delete-selected', [ProdukController::class, 'deleteSelected'])->name('produk.delete_selected');
-        Route::post('/produk/cetak-barcode', [ProdukController::class, 'cetakBarcode'])->name('produk.cetak_barcode');
-        Route::resource('/produk', ProdukController::class);
-
-        Route::get('/member/data', [MemberController::class, 'data'])->name('member.data');
-        Route::post('/member/cetak-member', [MemberController::class, 'cetakMember'])->name('member.cetak_member');
-        Route::resource('/member', MemberController::class);
-
         Route::get('/supplier/data', [SupplierController::class, 'data'])->name('supplier.data');
         Route::resource('/supplier', SupplierController::class);
 
         Route::get('/pengeluaran/data', [PengeluaranController::class, 'data'])->name('pengeluaran.data');
         Route::resource('/pengeluaran', PengeluaranController::class);
-
-        Route::get('/pembelian/data', [PembelianController::class, 'data'])->name('pembelian.data');
-        Route::get('/pembelian/{id}/create', [PembelianController::class, 'create'])->name('pembelian.create');
-        Route::resource('/pembelian', PembelianController::class)
-            ->except('create');
 
         Route::get('/pembelian_detail/{id}/data', [PembelianDetailController::class, 'data'])->name('pembelian_detail.data');
         Route::get('/pembelian_detail/loadform/{diskon}/{total}', [PembelianDetailController::class, 'loadForm'])->name('pembelian_detail.load_form');
@@ -68,22 +55,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/penjualan', [PenjualanController::class, 'index'])->name('penjualan.index');
         Route::get('/penjualan/{id}', [PenjualanController::class, 'show'])->name('penjualan.show');
         Route::delete('/penjualan/{id}', [PenjualanController::class, 'destroy'])->name('penjualan.destroy');
-    });
 
-    Route::group(['middleware' => 'level:1,2'], function () {
-        Route::get('/transaksi/baru', [PenjualanController::class, 'create'])->name('transaksi.baru');
-        Route::post('/transaksi/simpan', [PenjualanController::class, 'store'])->name('transaksi.simpan');
-        Route::get('/transaksi/selesai', [PenjualanController::class, 'selesai'])->name('transaksi.selesai');
-        Route::get('/transaksi/nota-kecil', [PenjualanController::class, 'notaKecil'])->name('transaksi.nota_kecil');
-        Route::get('/transaksi/nota-besar', [PenjualanController::class, 'notaBesar'])->name('transaksi.nota_besar');
-
-        Route::get('/transaksi/{id}/data', [PenjualanDetailController::class, 'data'])->name('transaksi.data');
-        Route::get('/transaksi/loadform/{diskon}/{total}/{diterima}', [PenjualanDetailController::class, 'loadForm'])->name('transaksi.load_form');
-        Route::resource('/transaksi', PenjualanDetailController::class)
-            ->except('create', 'show', 'edit');
-    });
-
-    Route::group(['middleware' => 'level:1'], function () {
         Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
         Route::get('/laporan/data/{awal}/{akhir}', [LaporanController::class, 'data'])->name('laporan.data');
         Route::get('/laporan/pdf/{awal}/{akhir}', [LaporanController::class, 'exportPDF'])->name('laporan.export_pdf');
@@ -99,14 +71,36 @@ Route::group(['middleware' => 'auth'], function () {
     Route::group(['middleware' => 'level:1,2'], function () {
         Route::get('/profil', [UserController::class, 'profil'])->name('user.profil');
         Route::post('/profil', [UserController::class, 'updateProfil'])->name('user.update_profil');
+
+        Route::get('/produk/data', [ProdukController::class, 'data'])->name('produk.data');
+        Route::post('/produk/delete-selected', [ProdukController::class, 'deleteSelected'])->name('produk.delete_selected');
+        Route::post('/produk/cetak-barcode', [ProdukController::class, 'cetakBarcode'])->name('produk.cetak_barcode');
+        Route::resource('/produk', ProdukController::class);
+
+        Route::get('/pembelian/data', [PembelianController::class, 'data'])->name('pembelian.data');
+        Route::get('/pembelian/{id}/create', [PembelianController::class, 'create'])->name('pembelian.create');
+        Route::resource('/pembelian', PembelianController::class)
+            ->except('create');
+
+        Route::get('/transaksi/baru', [PenjualanController::class, 'create'])->name('transaksi.baru');
+        Route::post('/transaksi/simpan', [PenjualanController::class, 'store'])->name('transaksi.simpan');
+        Route::get('/transaksi/selesai', [PenjualanController::class, 'selesai'])->name('transaksi.selesai');
+        Route::get('/transaksi/nota-kecil', [PenjualanController::class, 'notaKecil'])->name('transaksi.nota_kecil');
+        Route::get('/transaksi/nota-besar', [PenjualanController::class, 'notaBesar'])->name('transaksi.nota_besar');
+
+        Route::get('/transaksi/{id}/data', [PenjualanDetailController::class, 'data'])->name('transaksi.data');
+        Route::get('/transaksi/loadform/{diskon}/{total}/{diterima}', [PenjualanDetailController::class, 'loadForm'])->name('transaksi.load_form');
+        Route::resource('/transaksi', PenjualanDetailController::class)
+            ->except('create', 'show', 'edit');
     });
 });
-// Route::get('/keluar', function () {
-//     Auth::logout();
 
-//     request()->session()->invalidate();
+Route::get('/keluar', function () {
+    Auth::logout();
 
-//     request()->session()->regenerateToken();
+    request()->session()->invalidate();
 
-//     return redirect('/');
-// });
+    request()->session()->regenerateToken();
+
+    return redirect('/');
+});
